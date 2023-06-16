6c64e146b8285760d79c5892bb706455589a74cf
Daria Radu <dariaradur3@gmail.com>
2023-06-16 17:50:56 +0200

    Finish navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -31,26 +31,38 @@ const Navbar = () => {
     const [navbar, setNavbar] = useState(false)
 
     return (
-	<header className='w-full mx-auto px-4 bg-white shadow fixed top-0 z-50 dark:bg-stone-900 dark:border-b dark:border-stone-600'>
-        <div className='justify-between md:items-center md:flex'>
-			<div>
-				<div className='flex items-center justify-between py-3 md:py-5 md:block'>
-					<h2 className='text-2xl font-bold'>Daria Radu</h2>
-				</div>
-				<div className='md:hidden'>
-					<button>
-						{navbar ? <IoMdClose size={30}/> : <IoMdMenu size={30}/>}
-					</button>
-				</div>
+	<header className='w-full mx-auto px-20 bg-white shadow fixed top-0 z-50 dark:bg-stone-900 dark:border-b dark:border-stone-600'>
+        <div className='flex justify-between md:items-center'>
+			<div className='py-5 block'>
+				<h2 className='text-2xl font-bold'>Daria Radu</h2>
 			</div>
 
 			<div>
+				<div className='md:hidden py-5'>
+					<button onClick={() => setNavbar(!navbar)}>
+						{navbar ? <IoMdClose size={30}/> : <IoMdMenu size={30}/>}
+					</button>
+				</div>
+				
 				<div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
 					navbar ? "block" : "hidden"
 					}`}>
 					<div  className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
 						{NAV_ITEMS.map((item, idx) => {
-							return <a key={idx}>{item.label}</a>    
+							return (
+								<Link
+									key={idx}
+									to={item.page}
+									className={"block lg:inline-block text-neutral-900 hover:text-neutral-500 dark:text-neutral-100"}
+									activeClass='active'
+									spy={true}
+									offset={-100}
+									duration={500}
+									onClick={() => setNavbar(!navbar)}
+									>
+										{item.label}
+									</Link>
+							)    
 						})}
 						{currentTheme === 'dark' ? (
 						<button
